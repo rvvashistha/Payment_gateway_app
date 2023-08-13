f7ba2f16516eb8d739ed35191966b337d53f68ce
rvvashistha <rohitvashistha171@gmail.com>
2023-08-13 21:26:36 +0530

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const App = () => {
+    return (
+        <div>
+            Hello react app i will convet you to my good project!!!!
+        </div>
+    )
+}
+
+export default App
